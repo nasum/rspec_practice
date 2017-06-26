450ba999d2546f739ac50e0d2484935071485277
nasum <tomato.wonder.life@gmail.com>
2017-06-26 23:05:42 +0900

    add include context shared context

--- spec/user_spec.rb
+++ spec/user_spec.rb
@@ -2,32 +2,37 @@ require 'spec_helper'
 require './models/user.rb'
 
 RSpec.describe User do
-  describe 'greet' do
-    let(:user) { User.new(name: 'たろう', age: age) }
+  let(:user) { User.new(name: 'たろう', age: age) }
+  shared_context '12歳の場合' do
+    let(:age) { 12 }
+  end
+  shared_context '13歳の場合' do
+    let(:age) { 13 }
+  end
+
+  describe '#greet' do
     subject { user.greet }
 
-    shared_examples '子供のあいさつ' do
-      it { is_expected.to eq 'ぼくはたろうだよ。' }
-    end
-    context '0歳の場合' do
-      let(:age) { 0 }
-      it_behaves_like '子供のあいさつ'
-    end
     context '12歳以下の場合' do
-      let(:age) { 12 }
-      it_behaves_like '子供のあいさつ'
+      include_context '12歳の場合'
+      it { is_expected.to eq 'ぼくはたろうだよ。'}
     end
 
-    shared_examples '大人のあいさつ' do
-      it { is_expected.to eq '僕はたろうです。' }
-    end
     context '13以上の場合' do
-      let(:age) { 13 }
-      it_behaves_like '大人のあいさつ'
+      include_context '13歳の場合'
+      it { is_expected.to eq '僕はたろうです。'}
     end
-    context '100歳の場合' do
-      let(:age) { 100 }
-      it_behaves_like '大人のあいさつ'
+  end
+
+  describe '#child?' do
+    subject { user.child? }
+    context '12歳以下の場合' do
+      include_context '12歳の場合'
+      it { is_expected.to eq true }
+    end
+    context '12以上簿場合' do
+      include_context '13歳の場合'
+      it { is_expected.to eq false }
     end
   end
 end
