6f29011cb6c735b5de598d9a57885579dcdc5e6a
nasum <tomato.wonder.life@gmail.com>
2017-06-26 22:55:59 +0900

    use shared examples

--- spec/user_spec.rb
+++ spec/user_spec.rb
@@ -5,14 +5,29 @@ RSpec.describe User do
   describe 'greet' do
     let(:user) { User.new(name: 'たろう', age: age) }
     subject { user.greet }
-    context '12歳以下の場合' do
-      let(:age) { 12 }
+
+    shared_examples '子供のあいさつ' do
       it { is_expected.to eq 'ぼくはたろうだよ。' }
     end
-    
+    context '0歳の場合' do
+      let(:age) { 0 }
+      it_behaves_like '子供のあいさつ'
+    end
+    context '12歳以下の場合' do
+      let(:age) { 12 }
+      it_behaves_like '子供のあいさつ'
+    end
+
+    shared_examples '大人のあいさつ' do
+      it { is_expected.to eq '僕はたろうです。' }
+    end
     context '13以上の場合' do
       let(:age) { 13 }
-      it { is_expected.to eq '僕はたろうです。' }
+      it_behaves_like '大人のあいさつ'
+    end
+    context '100歳の場合' do
+      let(:age) { 100 }
+      it_behaves_like '大人のあいさつ'
     end
   end
 end
