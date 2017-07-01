75d047090a3b4b737874b840a7650ec8c88de1fb
nasum <tomato.wonder.life@gmail.com>
2017-07-01 11:55:07 +0900

    edit allow

--- spec/mock_spec.rb
+++ spec/mock_spec.rb
@@ -4,7 +4,7 @@ require './models/weather_bot.rb'
 RSpec.describe 'mock' do
   it 'エラーなく予報をツイートすること' do
     twitter_client_mock = double('Twitter Client')
-    allow(twitter_client_mock).to receive(:update)
+    expect(twitter_client_mock).to receive(:update)
 
     weather_bot = WeatherBot.new
     allow(weather_bot).to receive(:twitter_client).and_return(twitter_client_mock)
